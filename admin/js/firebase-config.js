a91c98048ff8421cc0b1c844be776cd437411437
PUGAZHenthi12 <100090207+PUGAZHenthi12@users.noreply.github.com>
2026-07-27 22:02:38 +0530

    Fix Firebase Firestore Import

--- admin/js/firebase-config.js
+++ admin/js/firebase-config.js
@@ -1,9 +1,9 @@
 // Firebase SDK
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
-import {
-    getAuth
-} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 // Firebase Configuration
 const firebaseConfig = {
