f8e59679d0a265ec85947914427f86b11c965c59
PUGAZHenthi12 <100090207+PUGAZHenthi12@users.noreply.github.com>
2026-07-27 13:02:52 +0530

    V4.3 - Firebase Configuration
    Added Firebase configuration and initialization code.

--- admin/js/firebase-config.js
+++ admin/js/firebase-config.js
@@ -1,1 +1,29 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
+import {
+    getAuth
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+// Firebase Configuration
+const firebaseConfig = {
+
+    apiKey: "AIzaSyAHfKtPGmkh4Biw-i2MYRv_KbzVZarIMwM",
+
+    authDomain: "lions-sports-wear.firebaseapp.com",
+
+    projectId: "lions-sports-wear",
+
+    storageBucket: "lions-sports-wear.firebasestorage.app",
+
+    messagingSenderId: "131458293230",
+
+    appId: "1:131458293230:web:ac28f4c2afd9d92a1d82ff"
+
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export Auth
+export const auth = getAuth(app);
